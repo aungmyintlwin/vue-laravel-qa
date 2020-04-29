19f3a1f2c588dcfe48f4df0fff372579aa7b67fd
jaspercoolcodex <jaspercool.codex@gmail.com>
2020-04-29 20:09:12 +0630

    lesson2

---
+++ app/Question.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'views',
+        'answers',
+        'votes',
+        'best_answer_id',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+}
