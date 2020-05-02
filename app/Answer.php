16c65556555ffb6ecadcf33494c764da3ffcdac1
jaspercoolcodex <jaspercool.codex@gmail.com>
2020-05-03 00:15:12 +0630

    Vote Accept

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\User;
+use App\Answer;
 use App\Question;
 use Illuminate\Database\Eloquent\Model;
 
@@ -38,6 +39,14 @@ class Answer extends Model
 
     public function getStatusAttribute()
     {
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
+    }
+    public function getIsBestAttribute()
+    {
+        return $this->isBest();
+    }
+    public function isBest()
+    {
+        return $this->id === $this->question->best_answer_id;
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,9 +17,20 @@
                             <a title="This Answer is not useful" class="vote-dwon off">
                                 <i class="fas fa-caret-down fa-3x" aria-hidden="true"></i>
                             </a>
-                            <a title="Mars this answer as best answer" class="{{$answer->status}} mt-2">
-                                <i class="fas fa-check fa-2x" aria-hidden="true"></i>
-                            </a>
+                            @can('accept',$answer)
+                                <a title="Mars this answer as best answer" class="{{$answer->status}} mt-2" onClick="event.preventDefault();document.getElementById('accept-answer-{{$answer->id}}').submit();">
+                                    <i class="fas fa-check fa-2x" aria-hidden="true"></i>
+                                </a>
+                                <form id="accept-answer-{{$answer->id}}" action="{{route('answers.accept',$answer->id)}}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            @else
+                                @if($answer->is_best)
+                                    <a title="The Question owner is accept this  as best answer" class="{{$answer->status}} mt-2">
+                                        <i class="fas fa-check fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             @parsedown($answer->body)
