42e161fffd7859e710a4376e0a1a6cd0b76448ed
jaspercoolcodex <jaspercool.codex@gmail.com>
2020-05-07 00:50:07 +0630

    create answer component

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -1,3 +1,38 @@
+<template>
+    <div class="media post">
+        <vote :model="answer" name="answer"></vote>
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update">
+                <div class="form-group">
+                    <textarea class="form-control" rows="10" v-model="body"></textarea>
+                </div>
+                <button class="btn btn-primary" :disabled="inInvalid">Update </button>
+                <button @click.prevent="cancle" class="btn btn-secondary">Cancle </button>
+            </form>
+            <div v-else>
+                <div v-html="body"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            <a v-if="authorize('modify',answer)" @click.prevent="edit" class="btn btn-outline-info">
+                                Edit
+                            </a>
+                            <button v-if="authorize('modify',answer)" class="btn btn-outline-danger" @click="destroy">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-4">
+        
+                    </div>
+                    <div class="col-4">
+                        <user-info :model="answer" label="Answered"></user-info>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 <script>
     export default {
         props: ['answer'],
