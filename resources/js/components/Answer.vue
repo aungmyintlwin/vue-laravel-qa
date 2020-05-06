a1d0c2652b8e4a13a083fc81d0e2d46142a40bbe
jaspercoolcodex <jaspercool.codex@gmail.com>
2020-05-07 02:21:56 +0630

    Implement custom event

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -79,9 +79,7 @@
                 
                             axios.delete(this.endpoint)
                                 .then(res => {
-                                    $(this.$el).fadeOut(500, ()=>{
-                                        this.$toast.success(res.data.message,'success',{timeout: 3000})
-                                    })
+                                    this.$emit('deleted');
                                 })
                 
                         }, true],
